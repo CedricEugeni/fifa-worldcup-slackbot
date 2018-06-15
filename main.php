4a47e56d8256143d5f190bd72f6c1d0a19b07ce7
Cedric Eugeni <ceugeni@molotov.tv>
2018-06-15 12:51:37 +0200

    Added code to send message to slack channel

--- main.php
+++ main.php
@@ -1,3 +1,30 @@
 <?php
 echo('hello world');
+
+const SLACK_TOKEN      = '';
+const SLACK_CHANNEL    = '#worldcup';
+const SLACK_BOT_NAME   = 'WorldCup Bot';
+const SLACK_BOT_AVATAR = 'http://i.imgur.com/LWw0Yoo.png';
+
+function postToSlack($text, $attachments_text = '')
+{
+  $slackUrl = 'https://slack.com/api/chat.postMessage?token='.SLACK_TOKEN.
+    '&channel='.urlencode(SLACK_CHANNEL).
+    '&username='.urlencode(SLACK_BOT_NAME).
+    '&icon_url='.SLACK_BOT_AVATAR.
+    '&unfurl_links=1&parse=full&pretty=1'.
+    '&text='.urlencode($text);
+  if ($attachments_text)
+  {
+    $slackUrl .= '&attachments='.urlencode('[{"text": "'.$attachments_text.'"}]');
+  }
+
+  $ch = curl_init($slackUrl);
+
+  curl_exec($ch);
+  curl_close($ch);
+}
+
+postToSlack('hello form slack bot');
+
 ?>
